a92b306c89b0915d86edf957b65d0432a7f6ea98
XYU <138056162+dgjwkwjsj@users.noreply.github.com>
2026-06-06 02:37:32 +0300

    commit

--- URL Interactable Grass/Assets/Scripts/Grass.cs
+++ URL Interactable Grass/Assets/Scripts/Grass.cs
@@ -1,15 +1,47 @@
 using UnityEngine;
+//using UnityEngine.GUIUtiling:ProcessEvent;
+using System.Collections.Generic;
 
 public class Grass : MonoBehaviour
 {
-    void Start()
+    Material grassMat;
+    MeshRenderer meshRenderer;
+    List<Transform> entities = new List<Transform>();
+    bool emptyList = true;
+    float pos = 0f;
+
+    private void Awake()
     {
-        
+        meshRenderer = GetComponent<MeshRenderer>();
+        grassMat = meshRenderer.material;
     }
 
-
-    void Update()
+    private void FixedUpdate()
     {
-        
+        if(entities.Count <= 0)
+        {
+            if (!emptyList)
+            {
+                Vector3 currentGrassMatPos = grassMat.GetVector("_Pos");
+                grassMat.SetVector("_Pos", currentGrassMatPos + new Vector3(0f, pos, 0f));
+                if (pos >= 50f) emptyList = true;
+                pos += 0.0025f;
+            }
+            return;
+        }
+        if (entities[entities.Count - 1] == null) return;
+        grassMat.SetVector("_Pos", entities[entities.Count - 1].position);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+       entities.Add(other.transform);
+        emptyList = false;
+        pos = 0f;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        entities.Remove(other.transform);
     }
 }
